72517b17ef047768e8af84b5754a779a06f76d34
artistic-differences <ross_smurfille@yahoo.co.uk>
2022-02-12 17:33:45 +0000

    Add list / view routing

---
+++ src/shemas/list.js
@@ -0,0 +1,13 @@
+//idea of what we might choose to store / configure in the list UX
+//some of this might actually go into the vm 
+//have a lot of flexibility between the two.
+const listUX = 
+{
+    "rowKey": "id",
+    "resouce": "person",
+    "shownFieldNames": ["firstName", "lastName", "startDate"],
+    "actions" : [{"name": "function"}],
+    "shownFields" : [
+        {"fieldName": "control"}
+    ]
+};
